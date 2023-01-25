8ce4d60f7fddb0a16341bafdfaa5154aa158d3f2
Kassadilla <kassadilla@Strattons-MacBook-Air.local>
2023-01-24 17:39:33 -0700

    8th try

--- src/image_output.cpp
+++ src/image_output.cpp
@@ -5,7 +5,7 @@ void drawAsciiImage( std::istream& is, std::ostream& os, const Image& image ) {
     int row = image.getHeight();
     int column = image.getWidth();
     for (int i = 0; i < row; i++){
-        for (int i2 = 0; i < column; i2++){
+        for (int i2 = 0; i2 < column; i2++){
             int red_val = image.getChannel(i, i2, 0);
             int green_val = image.getChannel(i, i2, 1);
             int blue_val = image.getChannel(i, i2, 2);
