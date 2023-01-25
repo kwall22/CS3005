f7d28cd4fc58fd473ae742341017162ed8eb4557
Kassadilla <kassadilla@Strattons-MacBook-Air.local>
2023-01-24 17:08:41 -0700

    4th try

--- src/image_drawing.cpp
+++ src/image_drawing.cpp
@@ -7,21 +7,42 @@ void diagonalQuadPattern( std::istream& is, std::ostream& os, Image& image ) {
     int width = getInteger(is, os, "Image width? ");
     image.setHeight(height);
     image.setWidth(width);
-    for (int i = 0; i < height; i++){
+
+    int i = height/2;
+    int i2 = 0;
+
+    for (;i < height; i++){
         for (int i2 = 0; i2 < width; i2++){
-            if (i < height/2){
-                image.setChannel(i, i2, 0, 0);
-            }
-            else{
-                image.setChannel(i, i2, 0, 255);
-            }        
-            if (i2 < width/2){
-                image.setChannel(i, i2, 2, 0);
-            }
-            else{
-                image.setChannel(i, i2, 2, 255);
-            }
-            image.setChannel(i, i2, 1, ( 2*i + 2*i2 ) % 256 );
+            image.setChannel(i, i2, 0, 255);
+        } 
+    } 
+    for(i = 0; i < height; i++){
+        for(i2 = 0; i2 < width; i2++){
+           image.setChannel(i, i2, 1, ( 2*i + 2*i2 ) % 256 ); 
+        }
+    }
+
+    for(i = 0; i < height; i++){
+        i2 = width/2;
+        for (;i2 < width; i2++){
+            image.setChannel(i, i2, 2, 255);
         }
     }
 }
+
+
+
+
+
+    // for (int i = 0; i2 > width/2; i2++) {
+    //    image.setChannel(i, i2, 2, 255); 
+    // }     
+    //     if (i2 < width/2){
+    //             image.setChannel(i, i2, 2, 0);
+    //         }
+    //         else{
+    //             image.setChannel(i, i2, 2, 255);
+    //         }
+    //         image.setChannel(i, i2, 1, ( 2*i + 2*i2 ) % 256 );
+    //     }
+    // }}
